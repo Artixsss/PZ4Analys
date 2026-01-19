fc7157d6cc674e0b070a605d9867593fce2bed78
Artixs <Artomixyn@gmail.com>
2026-01-20 04:51:12 +0700

    Sample app

--- src/Todo.SampleApp/Program.cs
+++ src/Todo.SampleApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using Todo.Core;
+
+var list = new TodoList();
+
+_ = list.Add("Buy milk");
+_ = list.Add("Read book");
+
+Console.WriteLine("All tasks:");
+foreach (var item in list.Items)
+{
+    Console.WriteLine($"- {item.Title} (done: {item.IsDone})");
+}
+
+Console.WriteLine();
+Console.WriteLine("Search: 'buy'");
+foreach (var item in list.Find("buy"))
+{
+    Console.WriteLine($"- {item.Title}");
+}
