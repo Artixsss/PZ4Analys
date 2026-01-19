73e0e64ef78d786fac86c81c3244e11b75ae3606
Artixs <Artomixyn@gmail.com>
2026-01-20 04:50:24 +0700

    Core todo classes

---
+++ src/Todo.Core/TodoItem.cs
@@ -0,0 +1,32 @@
+namespace Todo.Core
+{
+    using System;
+
+    public class TodoItem
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+
+        public string Title { get; private set; }
+
+        public bool IsDone { get; private set; }
+
+        public TodoItem(string title)
+        {
+            this.Title = title?.Trim() ?? throw new ArgumentNullException(nameof(title));
+        }
+
+        public void MarkDone() => this.IsDone = true;
+
+        public void MarkUndone() => this.IsDone = false;
+
+        public void Rename(string newTitle)
+        {
+            if (string.IsNullOrWhiteSpace(newTitle))
+            {
+                throw new ArgumentException("Title is required", nameof(newTitle));
+            }
+
+            this.Title = newTitle.Trim();
+        }
+    }
+}
---
+++ src/Todo.Core/TodoList.cs
@@ -0,0 +1,33 @@
+namespace Todo.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class TodoList
+    {
+        private readonly List<TodoItem> items = new();
+
+        public IReadOnlyList<TodoItem> Items => this.items.AsReadOnly();
+
+        public int Count => this.items.Count;
+
+        public TodoItem Add(string title)
+        {
+            TodoItem item = new(title);
+            this.items.Add(item);
+            return item;
+        }
+
+        public bool Remove(Guid id)
+        {
+            return this.items.RemoveAll(i => i.Id == id) > 0;
+        }
+
+        public IEnumerable<TodoItem> Find(string substring)
+        {
+            return this.items.Where(
+                i => i.Title.Contains(substring ?? string.Empty, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
